54b7db2204f5e496ec0a362c426bf07b8b4aa09b
Adam Cmiel <acmiel@redhat.com>
2022-09-09 13:25:36 +0200

    Pin atomic-reactor revision
    Make sure the test keeps working
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- runtest.sh
+++ runtest.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 
-rm -rf atomic-reactor-test/atomic-reactor
+repo_dir=atomic-reactor-test/atomic-reactor
+rm -rf "$repo_dir"
 
 git clone --single-branch --depth 1 \
     https://github.com/containerbuildsystem/atomic-reactor.git \
-    atomic-reactor-test/atomic-reactor
+    "$repo_dir"
+
+pushd "$repo_dir"
+git checkout d59e7b941aebe33cf2855483eba96dbfb2208c93
+popd
 
 venv/bin/cachitool \
     --package '{
         "type":"pip",
-        "path": "atomic-reactor-test/atomic-reactor",
+        "path": "'"$repo_dir"'",
         "requirements_build_files": ["requirements-build.txt", "requirements-pip.txt"]
     }' \
     --workdir atomic-reactor-test/workdir
